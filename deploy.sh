9df8c9c34a8985e82d75909f2792e1bfce10f736
Steve Davies <steve@connection-telecom.com>
2015-10-25 15:07:34 +0200

    Actually push to AWS

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ rm -rf dist/install 2>/dev/null
 mkdir dist/install
 npm install --prefix dist/install .
 rm dist/dist.zip
-zip dist/dist.zip ./dist/install/node_modules/alexa-plex/*
+(cd dist/install/node_modules/alexa-plex; zip -r ../../../dist.zip * .env)
 echo Function zipped. Updating...
-# aws lambda update-function-code --zip-file fileb://dist\dist.zip --function-name alexa-plex
+aws lambda update-function-code --zip-file fileb://./dist/dist.zip --function-name alexa-plex
